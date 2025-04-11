5c83b6e0bb8a0c207b9663399e7c6ad105c96cca
Davi Costa de Melo <davicosta01082008@gmail.com>
2025-04-11 00:50:37 -0300

    feat: enable cors

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use server::{
 };
 use std::sync::Arc;
 use tokio::sync::RwLock;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
@@ -16,6 +17,12 @@ async fn main() {
   let app = Router::new()
     .route("/rooms/{room_id}", get(enter_room::handle))
     .route("/rooms", post(create_room::handle))
+    .layer(
+      CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any),
+    )
     .with_state(state.clone());
 
   let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
